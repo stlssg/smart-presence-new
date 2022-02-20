7a889421df6562390655777ba4b8535fc1670fd0
stlssg <stlssg@gmail.com>
2022-02-21 00:28:11 +0100

    use previous one for now

--- app/src/main/java/it/polito/interdisciplinaryProjects2021/smartpresence/presenceDetection/PresenceDetectionFragment.kt
+++ app/src/main/java/it/polito/interdisciplinaryProjects2021/smartpresence/presenceDetection/PresenceDetectionFragment.kt
@@ -27,72 +27,72 @@ class PresenceDetectionFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val blurView = view.findViewById<BlurView>(R.id.blurView)
-        blurBackground(blurView)
-
-        val wifiCheckingGuidanceCard = view.findViewById<CardView>(R.id.wifiCheckingGuidanceCard)
-        val positioningCheckingGuidanceCard = view.findViewById<CardView>(R.id.positioningCheckingGuidanceCard)
-        val manualCheckingGuidanceCard = view.findViewById<CardView>(R.id.manualCheckingGuidanceCard)
-
-        val wifiCheckingGuidance = view.findViewById<ScrollView>(R.id.wifiCheckingGuidance)
-        val positioningCheckingGuidance = view.findViewById<ScrollView>(R.id.positioningCheckingGuidance)
-        val manualCheckingGuidance = view.findViewById<ScrollView>(R.id.manualCheckingGuidance)
+//        val blurView = view.findViewById<BlurView>(R.id.blurView)
+//        blurBackground(blurView)
+//
+//        val wifiCheckingGuidanceCard = view.findViewById<CardView>(R.id.wifiCheckingGuidanceCard)
+//        val positioningCheckingGuidanceCard = view.findViewById<CardView>(R.id.positioningCheckingGuidanceCard)
+//        val manualCheckingGuidanceCard = view.findViewById<CardView>(R.id.manualCheckingGuidanceCard)
+//
+//        val wifiCheckingGuidance = view.findViewById<ScrollView>(R.id.wifiCheckingGuidance)
+//        val positioningCheckingGuidance = view.findViewById<ScrollView>(R.id.positioningCheckingGuidance)
+//        val manualCheckingGuidance = view.findViewById<ScrollView>(R.id.manualCheckingGuidance)
 
         val goToWifiChecking = view.findViewById<CardView>(R.id.wifiChecking)
         val goToPositioningChecking = view.findViewById<CardView>(R.id.positioningChecking)
         val goToManualChecking = view.findViewById<CardView>(R.id.manualChecking)
 
-        blurView.setOnClickListener {
-            TransitionManager.beginDelayedTransition(wifiCheckingGuidanceCard, AutoTransition())
-            TransitionManager.beginDelayedTransition(positioningCheckingGuidanceCard, AutoTransition())
-            TransitionManager.beginDelayedTransition(manualCheckingGuidanceCard, AutoTransition())
-            it.visibility = View.GONE
-            wifiCheckingGuidance.visibility = View.GONE
-            positioningCheckingGuidance.visibility = View.GONE
-            manualCheckingGuidance.visibility = View.GONE
-        }
+//        blurView.setOnClickListener {
+//            TransitionManager.beginDelayedTransition(wifiCheckingGuidanceCard, AutoTransition())
+//            TransitionManager.beginDelayedTransition(positioningCheckingGuidanceCard, AutoTransition())
+//            TransitionManager.beginDelayedTransition(manualCheckingGuidanceCard, AutoTransition())
+//            it.visibility = View.GONE
+//            wifiCheckingGuidance.visibility = View.GONE
+//            positioningCheckingGuidance.visibility = View.GONE
+//            manualCheckingGuidance.visibility = View.GONE
+//        }
 
         goToWifiChecking.setOnClickListener {
             findNavController().navigate(R.id.wifiCheckingFragment)
         }
-        goToWifiChecking.setOnLongClickListener {
-            TransitionManager.beginDelayedTransition(wifiCheckingGuidanceCard, AutoTransition())
-            blurView.visibility = View.VISIBLE
-            wifiCheckingGuidance.visibility = View.VISIBLE
-            true
-        }
+//        goToWifiChecking.setOnLongClickListener {
+//            TransitionManager.beginDelayedTransition(wifiCheckingGuidanceCard, AutoTransition())
+//            blurView.visibility = View.VISIBLE
+//            wifiCheckingGuidance.visibility = View.VISIBLE
+//            true
+//        }
 
         goToPositioningChecking.setOnClickListener {
             findNavController().navigate(R.id.positioningCheckingFragment)
         }
-        goToPositioningChecking.setOnLongClickListener {
-            TransitionManager.beginDelayedTransition(positioningCheckingGuidanceCard, AutoTransition())
-            blurView.visibility = View.VISIBLE
-            positioningCheckingGuidance.visibility = View.VISIBLE
-            true
-        }
+//        goToPositioningChecking.setOnLongClickListener {
+//            TransitionManager.beginDelayedTransition(positioningCheckingGuidanceCard, AutoTransition())
+//            blurView.visibility = View.VISIBLE
+//            positioningCheckingGuidance.visibility = View.VISIBLE
+//            true
+//        }
 
         goToManualChecking.setOnClickListener {
             findNavController().navigate(R.id.manualCheckingFragment)
         }
-        goToManualChecking.setOnLongClickListener {
-            TransitionManager.beginDelayedTransition(manualCheckingGuidanceCard, AutoTransition())
-            blurView.visibility = View.VISIBLE
-            manualCheckingGuidance.visibility = View.VISIBLE
-            true
-        }
+//        goToManualChecking.setOnLongClickListener {
+//            TransitionManager.beginDelayedTransition(manualCheckingGuidanceCard, AutoTransition())
+//            blurView.visibility = View.VISIBLE
+//            manualCheckingGuidance.visibility = View.VISIBLE
+//            true
+//        }
     }
 
-    private fun blurBackground(blurView: BlurView) {
-        val decorView = activity?.window?.decorView
-        val rootView = decorView?.findViewById<ViewGroup>(android.R.id.content)
-        val windowBackground = decorView?.background
-
-        blurView.setupWith(rootView!!)
-            .setFrameClearDrawable(windowBackground)
-            .setBlurAlgorithm(RenderScriptBlur(requireContext()))
-            .setBlurRadius(10f)
-            .setBlurAutoUpdate(true)
-            .setHasFixedTransformationMatrix(true)
-    }
+//    private fun blurBackground(blurView: BlurView) {
+//        val decorView = activity?.window?.decorView
+//        val rootView = decorView?.findViewById<ViewGroup>(android.R.id.content)
+//        val windowBackground = decorView?.background
+//
+//        blurView.setupWith(rootView!!)
+//            .setFrameClearDrawable(windowBackground)
+//            .setBlurAlgorithm(RenderScriptBlur(requireContext()))
+//            .setBlurRadius(10f)
+//            .setBlurAutoUpdate(true)
+//            .setHasFixedTransformationMatrix(true)
+//    }
 }
