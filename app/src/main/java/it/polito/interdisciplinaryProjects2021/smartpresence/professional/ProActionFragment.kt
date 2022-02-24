531e68371557d970531d6f03f8a3b190fab08fa1
stlssg <stlssg@gmail.com>
2022-02-24 20:56:23 +0100

    keep improving & previous for now

--- app/src/main/java/it/polito/interdisciplinaryProjects2021/smartpresence/professional/ProActionFragment.kt
+++ app/src/main/java/it/polito/interdisciplinaryProjects2021/smartpresence/professional/ProActionFragment.kt
@@ -1,5 +1,8 @@
 package it.polito.interdisciplinaryProjects2021.smartpresence.professional
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -21,18 +24,42 @@ class ProActionFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        val currentNumberOccupants = view.findViewById<TextView>(R.id.currentNumberOccupants)
-//        currentNumberOccupants.text = "2"
-//        val selectedHourlyOccupancy = view.findViewById<TextView>(R.id.selectedHourlyOccupancy)
-//        selectedHourlyOccupancy.text = "0.8"
-//        val allHourlyOccupancy = view.findViewById<TextView>(R.id.allHourlyOccupancy)
-//        allHourlyOccupancy.text = "0.9"
+//        val sharedPreferences: SharedPreferences = requireContext().getSharedPreferences("AppSharedPreference", Context.MODE_PRIVATE)
 //
-//        val selectedHourlyOccupancyBar = view.findViewById<CircularProgressBar>(R.id.selectedHourlyOccupancyBar)
-//        setProgressBar(selectedHourlyOccupancyBar, 0.8f)
+//        val address = sharedPreferences.getString("address", "nothing")
+//        val latitude = sharedPreferences.getString("latitude", "nothing")
+//        val longitude = sharedPreferences.getString("longitude", "nothing")
+//        val ssid = sharedPreferences.getString("ssid", "nothing")
+//        val bssid = sharedPreferences.getString("bssid", "nothing")
+//        val maxOccupancy = sharedPreferences.getString("maxOccupancy", "nothing")
 //
-//        val allHourlyOccupancyBar = view.findViewById<CircularProgressBar>(R.id.allHourlyOccupancyBar)
-//        setProgressBar(allHourlyOccupancyBar, 0.9f)
+//        val infoProAddress = view.findViewById<TextView>(R.id.infoProAddress)
+//        val infoProLat = view.findViewById<TextView>(R.id.infoProLat)
+//        val infoProLon = view.findViewById<TextView>(R.id.infoProLon)
+//        val infoProSSID = view.findViewById<TextView>(R.id.infoProSSID)
+//        val infoProBSSID = view.findViewById<TextView>(R.id.infoProBSSID)
+//        val infoProMax = view.findViewById<TextView>(R.id.infoProMax)
+//
+//        // need change
+//        assignContentForBuildingInfo(address?.replace("_", " "), "Address", infoProAddress)
+//        assignContentForBuildingInfo(latitude, "Latitude", infoProLat)
+//        assignContentForBuildingInfo(longitude, "Longitude", infoProLon)
+//        assignContentForBuildingInfo(ssid, "SSID", infoProSSID)
+//        assignContentForBuildingInfo(bssid, "BSSID", infoProBSSID)
+//        assignContentForBuildingInfo(maxOccupancy, "Maximum expected occupants", infoProMax)
+
+        val currentNumberOccupants = view.findViewById<TextView>(R.id.currentNumberOccupants)
+        currentNumberOccupants.text = "2"
+        val selectedHourlyOccupancy = view.findViewById<TextView>(R.id.selectedHourlyOccupancy)
+        selectedHourlyOccupancy.text = "0.8"
+        val allHourlyOccupancy = view.findViewById<TextView>(R.id.allHourlyOccupancy)
+        allHourlyOccupancy.text = "0.9"
+
+        val selectedHourlyOccupancyBar = view.findViewById<CircularProgressBar>(R.id.selectedHourlyOccupancyBar)
+        setProgressBar(selectedHourlyOccupancyBar, 0.8f)
+
+        val allHourlyOccupancyBar = view.findViewById<CircularProgressBar>(R.id.allHourlyOccupancyBar)
+        setProgressBar(allHourlyOccupancyBar, 0.9f)
 
     }
 
@@ -49,13 +76,22 @@ class ProActionFragment : Fragment() {
             backgroundProgressBarColor = Color.GRAY
 
             // Set Width
-            progressBarWidth = 5f // in DP
-            backgroundProgressBarWidth = 8f // in DP
+            progressBarWidth = 6f // in DP
+            backgroundProgressBarWidth = 12f // in DP
 
             // Other
             roundBorder = true
             startAngle = 0f
             progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun assignContentForBuildingInfo(stringInput: String?, additionalString: String, tv: TextView) {
+        if (stringInput == "nothing") {
+            tv.text = "$additionalString: Not available" // need change
+        } else {
+            tv.text = "$additionalString: $stringInput"
         }
     }
 
